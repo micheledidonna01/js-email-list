dea92a79a19a81090aac854666467f593a3b918c
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-13 16:24:08 +0100

    add: update syntax

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
 let listEmail = document.getElementById('list-email');
 let btnChange = document.getElementById('btn-change');
 btnChange.addEventListener('click', changeEmail);
+
 let api = 'https://flynn.boolean.careers/exercises/api/random/mail';
 let items = '';
-for (let i = 0; i < 10; i++) {
-    axios.get(api)
-        .then(response => {
-            const result = response.data;
-            console.log(result);
-            if (i === 0) {
-            listEmail.innerHTML += `<li class="list-group-item active">${result.response} </li>`;
-            }else{
-            listEmail.innerHTML += `<li class="list-group-item">${result.response} </li>`;
-            }
-        })
-        .catch(error => {
-            console.error(error);
-        })
 
-}
+generatorEmail();
 
-
-function changeEmail(){
-    listEmail.innerHTML = '';
+function generatorEmail(){
     for (let i = 0; i < 10; i++) {
         axios.get(api)
             .then(response => {
@@ -39,4 +24,9 @@ function changeEmail(){
             })
     
     }
+}
+
+function changeEmail(){
+    listEmail.innerHTML = '';
+    generatorEmail();
 }
