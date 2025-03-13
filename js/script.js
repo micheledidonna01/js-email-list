3089a6fcc1d2d0ecc03931bcc00789270adf22fb
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-13 15:46:17 +0100

    feat: bonus 1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 let listEmail = document.getElementById('list-email');
 let api = 'https://flynn.boolean.careers/exercises/api/random/mail';
 let items = '';
-for(let i=0; i<10; i++){
+for (let i = 0; i < 10; i++) {
     axios.get(api)
         .then(response => {
             const result = response.data;
             console.log(result);
-            listEmail.innerHTML += `<li>Email ${i+1}: ${result.response} </li>`;
+            if (i === 0) {
+            listEmail.innerHTML += `<li class="list-group-item active">${result.response} </li>`;
+            }else{
+            listEmail.innerHTML += `<li class="list-group-item">${result.response} </li>`;
+            }
         })
         .catch(error => {
             console.error(error);
         })
-    
+
 }
 
