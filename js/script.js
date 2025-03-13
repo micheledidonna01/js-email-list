0daad9808b3cd6eda0519bfc426dcdf23428e403
micheledidonna01 <micheledidonna337@gmail.com>
2025-03-13 15:54:50 +0100

    feat: bonus 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 let listEmail = document.getElementById('list-email');
+let btnChange = document.getElementById('btn-change');
+btnChange.addEventListener('click', changeEmail);
 let api = 'https://flynn.boolean.careers/exercises/api/random/mail';
 let items = '';
 for (let i = 0; i < 10; i++) {
@@ -18,3 +20,23 @@ for (let i = 0; i < 10; i++) {
 
 }
 
+
+function changeEmail(){
+    listEmail.innerHTML = '';
+    for (let i = 0; i < 10; i++) {
+        axios.get(api)
+            .then(response => {
+                const result = response.data;
+                console.log(result);
+                if (i === 0) {
+                listEmail.innerHTML += `<li class="list-group-item active">${result.response} </li>`;
+                }else{
+                listEmail.innerHTML += `<li class="list-group-item">${result.response} </li>`;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    
+    }
+}
